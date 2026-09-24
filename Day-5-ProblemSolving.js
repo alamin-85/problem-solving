83e243bdbe80977e1559023d1bfc02a3faa5b62e
arifislam121416 <ardairyhelthcare@gmail.com>
2026-09-24 18:49:52 +0600

    problem 22 complete

--- Day-5-ProblemSolving.js
+++ Day-5-ProblemSolving.js
@@ -7,3 +7,22 @@ function factorial(n){
     }
 }
 // console.log(factorial(5));
+
+// Problem 22
+function fibonacci(n){
+    if(n=== 0){
+        return 0
+    } else if(n===1){
+        return  1
+    }
+    let a = 0;
+    let b =1;
+for(let i = 2;i<=n ;i++){
+    let result = a + b;
+    a = b ;
+    b = result ;
+
+}
+return b
+}
+// console.log(fibonacci(6));
